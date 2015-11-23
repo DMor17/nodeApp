5791779905268ef523faaf42a7af880994c7021a
dmor17 <broady71>
2015-11-23 15:28:32 +0000

    Added code to add 3 users and print out a success message.

--- insert.js
+++ insert.js
@@ -27,11 +27,31 @@ http.createServer(function(request, response) {
             //HURRAY!! We are connected. :)
             response.write('Connection established to' + url +"\n");
 
-            // do some work here with the database.
+            // Get the documents collection
+            var collection = db.collection('users');
 
-            //Done Close connection
-            db.close();
+            // Creates a JSON user object with various fields
+            var user1 = {name: 'modulus admin', age: 42, roles: ['admin', 'moderator', 'user']};
+            var user2 = {name: 'modulus user', age: 22, roles: ['user']};
+            var user3 = {name: 'modulus super admin', age: 92, roles: ['super-admin', 'admin', 'moderator', 'user']};
+
+            // Insert some users
+            collection.insert([user1, user2, user3], function (err, result) {
+                if (err) {
+                    response.write('Insert failed ' + err + "\n");
+                } else {
+                    console.log(result);
+                    response.write('Inserted ' + result.insertedCount +' documents ok. +"\n"');
+                }
+                //Close connection
+                db.close();
+                response.end('Finished, Connection closed \n');
+                //remove any other db.close or response.end statement below this line
+            });
+
+
         }
+        db.close();
         response.end('Finished, Connection closed \n');
     });
 
